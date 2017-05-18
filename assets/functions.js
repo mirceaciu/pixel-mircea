3e1c45d18786d738902c8803f32253cf461c7a40
mircea <mircea@icisoft.ro>
2017-05-18 14:37:07 +0300

    style fix

--- assets/functions.js
+++ assets/functions.js
@@ -2,6 +2,7 @@
 // Left Slider
 // #  # # # # # # # # #
 
+
 var swiperLeft = new Swiper(".ins-left", {
     // pagination: '.swiper-pagination',
     // prevButton: ".swiper-button-prev",
@@ -42,3 +43,32 @@ var swiperRight = new Swiper(".ins-right", {
       swiperLeft.lockSwipes(true);
     },
 });
+
+var autoSizeText;
+
+autoSizeText = function() {
+  var el, elements, _i, _len, _results;
+  elements = $('.resize');
+  console.log(elements);
+  if (elements.length < 0) {
+    return;
+  }
+  _results = [];
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+    el = elements[_i];
+    _results.push((function(el) {
+      var resizeText, _results1;
+      resizeText = function() {
+        var elNewFontSize;
+        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+        return $(el).css('font-size', elNewFontSize);
+      };
+      _results1 = [];
+      while (el.scrollHeight > el.offsetHeight) {
+        _results1.push(resizeText());
+      }
+      return _results1;
+    })(el));
+  }
+  return _results;
+};
